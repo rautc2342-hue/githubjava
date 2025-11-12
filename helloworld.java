9e03feb0aac40f2051b3e605f1fde41772dd4008
rautc2342-hue <rautc2342@gmail.com>
2025-11-12 16:26:13 +0530

    Add Simple class to print greetings for batch teams

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch40 team with Isar , Noman Amitabh");
+      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Myusr, Rohit, 3rd Commit Aditya in making CI automated vikas teach devops CI proecss");
+     System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
+      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
+    }  
+} 
